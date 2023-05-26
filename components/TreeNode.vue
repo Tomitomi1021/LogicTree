387f0a0169ec928d60f55ee12d5fc9e09456d026
Taiki Tomita <g.tomita.t@gmail.com>
2023-05-26 23:49:04 +0900

    hjklでフォーカスを移動できるようにした．

--- components/TreeNode.vue
+++ components/TreeNode.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="tree-node">
-    <div class="node-label">{{ node.label }}</div>
+    <div 
+      class="node-label"
+      tabindex='0'
+      @keydown="handleKeyDown"
+      ref="nodeLabel">{{ node.label }}</div>
     <div class="children">
-      <TreeNode v-for="childNode in node.children" :key="childNode.id" :node="childNode" />
+      <TreeNode 
+        v-for="(childNode,index) in node.children" 
+        :node="childNode" 
+        @activePrevSibling="()=>activateChildNode(index-1)"
+        @activeNextSibling="()=>activateChildNode(index+1)"
+        ref="childrenComponent"/>
     </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'TreeNode',
   props: {
@@ -16,6 +26,53 @@ export default {
       required: true,
     },
   },
+  data(){
+    return {
+      isActive:false
+    }
+  },
+  methods: {
+    handleKeyDown(event) {
+      if (event.key === 'h') {
+        this.activateParentNode();
+      } else if (event.key === 'l') {
+        this.activateFirstChildNode();
+      } else if (event.key === 'k') {
+        this.activatePreviousSiblingNode();
+      } else if (event.key === 'j') {
+        this.activateNextSiblingNode();
+      }
+    },
+    activate(){
+      this.$refs.nodeLabel.focus();
+    },
+    activateChildNode(index){
+      if(index<0||this.$refs.childrenComponent.length<=index)return;
+
+      const firstChildNode = this.$refs.childrenComponent[index];
+      if (firstChildNode && firstChildNode.activate) {
+        firstChildNode.activate();
+      }
+    },
+    activateParentNode() {
+      const parentNode = this.$parent;
+      if (parentNode && parentNode.activate) {
+        parentNode.activate();
+      }
+    },
+    activateFirstChildNode() {
+      const firstChildNode = this.$refs.childrenComponent[0];
+      if (firstChildNode && firstChildNode.activate) {
+        firstChildNode.activate();
+      }
+    },
+    activatePreviousSiblingNode() {
+      this.$emit("activePrevSibling");
+    },
+    activateNextSiblingNode() {
+      this.$emit("activeNextSibling");
+    }
+  }
 };
 </script>
 
@@ -26,6 +83,16 @@ export default {
 
 .node-label {
   font-weight: bold;
+}
+
+.node-label:focus{
+  font-weight: bold;
+  background-color: yellow;
+}
+
+.node-label-active{
+  font-weight: bold;
+  background-color: yellow;
 }
 
 .children {
