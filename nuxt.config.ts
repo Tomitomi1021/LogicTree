97a458e41265ab4deb683eed250cc9ea5cbc9ce1
Taiki Tomita <g.tomita.t@gmail.com>
2023-07-20 21:39:48 +0900

    nuxt.config.tsでconsole.logデバッグしてみる．

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+console.log("DEBUG HOGEHOGE");
+console.log(`proces.env?.BASE_URL = ${process.env?.BASE_URL}`);
 export default defineNuxtConfig({
   app: {
     baseURL: process.env?.BASE_URL,
